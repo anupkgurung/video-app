0637e35ce8ea0bd5e804920e7466daad3436bc70
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-05-18 23:13:26 +0530

    added home/video listing page

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,3 +1,21 @@
-export const VideoCard = () => {
-    
+
+import "./VideoCard.css"
+export const VideoCard = ({id,title,video,thumnail,creator,alt}) => {
+    return (
+        <>
+            <div className='w-20' id={id}>
+                    <div className="">
+                        <img className="w-100" src={thumnail} alt={alt} />
+                    </div>
+                  
+                    <div className="">
+                       {title}
+                    </div>
+                    
+                        <div className="footer-icons">
+                            <span className="material-icons">visibility</span>{creator}
+                        </div>             
+            </div>
+        </>
+    )
 }
