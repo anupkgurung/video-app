fdb889d5abca3cfa50b2bf0cb0cb3760e53e7204
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-05-29 01:32:22 +0530

    Added liked video Feature

--- src/pages/SingleVideo/SingleVideo.jsx
+++ src/pages/SingleVideo/SingleVideo.jsx
@@ -2,11 +2,17 @@ import axios from "axios";
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 import { Sidebar, VideoCard } from "../../components"
+import { usePlaylistContext } from "../../context";
+import { watchLater } from "../../Api";
 
 export const SingleVideo = () => {
     const { id: videoId } = useParams();
     const [video, setVideo] = useState({})
-    const encodedToken = localStorage.getItem('token')
+    const encodedToken = localStorage.getItem('token');
+    const { initialState, playlistDispatch } = usePlaylistContext() || {};
+    const { isPlaylistModalVisible } = initialState;
+    const [isLiked, setIsLiked] = useState(false);
+
     const getVideo = async () => {
         try {
             const data = await axios.get(`/api/video/${videoId}`)
@@ -26,6 +32,17 @@ export const SingleVideo = () => {
             console.log(error)
         }
     }
+
+    
+    const addToLikedVideo =  async() => {
+        try {
+            await axios.post("/api/user/likes",{video},
+            { headers: { authorization: encodedToken } })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     useEffect(() => {
         if(Object.keys(video).length === 0){
             getVideo()
@@ -55,16 +72,28 @@ export const SingleVideo = () => {
                         <div className="flex justify-spc-btwn pd-top">
                             <div className="text-bold">{video.creator}</div>
                             <div className="flex">
-                                <button className="no-border cursor-pointer flex align-center mr-2">
-                                    <span className="material-icons-outlined">thumb_up</span>LIKE
+                                <button className="no-border cursor-pointer flex align-center mr-2" onClick={()=>addToLikedVideo(video._id)}>
+                                    <span className={isLiked ? "material-icons" : "material-icons-outlined"}>thumb_up</span>LIKE
                                 </button>
                                 <button className="no-border cursor-pointer flex align-center mr-2">
                                     <span className="material-icons-outlined">thumb_down</span>DISLIKE
                                 </button>
-                                <button className="no-border cursor-pointer flex align-center mr-2">
+                                <button className="no-border cursor-pointer flex align-center mr-2"
+                                        onClick={
+                                            () => {
+                                                playlistDispatch({
+                                                    type: "SET_PLAYLIST_MODAL_VISIBLE",
+                                                    payload: isPlaylistModalVisible
+                                                })
+                                                playlistDispatch({
+                                                    type: "SET_PLAYLIST_VIDEO",
+                                                    payload: video
+                                                })
+                                            }
+                                        }>
                                     <span className="material-icons">{true ? 'playlist_add' : 'playlist_add_check'}</span>ADD TO PLAYLIST
                                 </button>
-                                <button className="no-border cursor-pointer flex align-center mr-2">
+                                <button className="no-border cursor-pointer flex align-center mr-2" onClick={() => watchLater(video)}>
                                     <span className="material-icons-outlined">{true ? 'watch_later' : 'task_alt'}</span>WATCH LATER
                                 </button>
                             </div>
