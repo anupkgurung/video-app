8507caad32059e9cfb41c80604590f3c838d4e40
nealcaffre <31470135+nealcaffre@users.noreply.github.com>
2022-06-06 12:44:48 +0530

    default user changed

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshBalika123",
+    firstName: "guest",
+    lastName: "user",
+    email: "guestuser@gmail.com",
+    password: "1234",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
